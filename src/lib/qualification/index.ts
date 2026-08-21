113bd43cf3ae7cd53b60336c90acf30e7f99b899
Hekmat Khan <hekmatk255@gmail.com>
2026-08-21 14:57:17 +0430

    Create src/lib/qualification/index.ts

---
+++ src/lib/qualification/index.ts
@@ -0,0 +1,16 @@
+export {
+  qualifyOpportunity,
+} from "./ai";
+
+export {
+  generateIcebreaker,
+} from "./icebreaker";
+
+export {
+  prepareQualification,
+} from "./prequalification";
+
+export type {
+  QualificationInput,
+  QualificationResult,
+} from "./types";
