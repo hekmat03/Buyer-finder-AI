76e9d9226ae1aeea3d5d6789b9e7b472460c84e6
Hekmat Khan <hekmatk255@gmail.com>
2026-08-21 14:57:52 +0430

    Create src/lib/opportunities/index.ts

---
+++ src/lib/opportunities/index.ts
@@ -0,0 +1,23 @@
+export {
+  filterOpportunities,
+} from "./filters";
+
+export {
+  sortOpportunities,
+} from "./sort";
+
+export {
+  calculateOpportunityStats,
+} from "./stats";
+
+export type {
+  OpportunityFilterOptions,
+} from "./filters";
+
+export type {
+  OpportunitySortMode,
+} from "./sort";
+
+export type {
+  OpportunityStats,
+} from "./stats";
