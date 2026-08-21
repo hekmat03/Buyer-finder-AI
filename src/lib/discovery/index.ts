6ac5346cc9eb803c7687b4511d0056b12ca0dad2
Hekmat Khan <hekmatk255@gmail.com>
2026-08-21 14:56:59 +0430

    Create src/lib/discovery/index.ts

---
+++ src/lib/discovery/index.ts
@@ -0,0 +1,31 @@
+export {
+  runDiscoveryPipeline,
+} from "./pipeline";
+
+export {
+  assessFreshness,
+} from "./freshness";
+
+export {
+  normalizeCandidate,
+  normalizeText,
+  canonicalizeUrl,
+  createTextHash,
+} from "./normalize";
+
+export {
+  findDuplicate,
+} from "./dedupe";
+
+export {
+  matchService,
+  matchAllServices,
+} from "./service-match";
+
+export {
+  assessBuyingIntent,
+} from "./buying-intent";
+
+export {
+  verifyCandidate,
+} from "./verification";
